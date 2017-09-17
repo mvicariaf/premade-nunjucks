c10aa892f3abdd3ff77eddb7a466f356c3a88863
mvicariaf <amivicaria@hotmail.es>
2017-09-18 01:51:15 +0200

    añadidas noticas a la pagina principal

---
+++ public/js/news.js
@@ -0,0 +1,11 @@
+$.get('https://newsapi.org/v1/articles?source=ign&sortBy=top&apiKey=0c3a214cc5274d77b6b9856dd3f9f28a', function (response){  
+	 	
+	 	for (var i = 0; i < response.articles.length; i++) {
+	 		
+	 		
+	 		$('.list-group').append("<a href='"+response.articles[i].url+"' class='list-group-item list-group-item-action'>" + response.articles[i].title  + "</a>");
+	 		
+	 		
+	 	};
+
+	});
